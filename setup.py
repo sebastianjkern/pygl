03b264bf2f0601b6c59deadce5e37f9605954b49
sebastianjkern <sebastianjakobkern@gmail.com>
2021-08-26 17:38:09 +0200

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pygl",
-    version="0.0.1",
+    version="0.0.2",
     description="Python wrapper code to run glsl shader in opengl window",
     url="git@github.com:sebastianjkern/pygl.git",
     author="sebastianjkern",
